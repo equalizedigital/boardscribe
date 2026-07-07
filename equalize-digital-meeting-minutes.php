9a065f8ee86f8c640ca4f63d23ca5c7ca50c6dfe
Steve Jones <steve@stevejonesdev.com>
2026-07-07 10:47:40 -0400

    fix: update tested up to version in plugin header to 7.0

--- equalize-digital-meeting-minutes.php
+++ equalize-digital-meeting-minutes.php
@@ -11,7 +11,7 @@
  * Text Domain:       edmm
  * Requires at least: 6.0
  * Requires PHP:      7.4
- * Tested up to:      6.7
+ * Tested up to:      7.0
  *
  * @package EqualizeDigital\MeetingMinutes
  */
